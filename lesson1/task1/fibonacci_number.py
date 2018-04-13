2b74d842013a3227f9f046aa63838b7d9a2ce0ef
Alexander S. Kulikov <alexander.s.kulikov@gmail.com>
2018-04-13 19:28:02 +0300

    add first problem (fibonacci number)

---
+++ lesson1/task1/fibonacci_number.py
@@ -0,0 +1,21 @@
+#python3 (this comment is needed by the autograding system at Coursera and edX)
+
+def fibonacci_number_naive(n):
+    assert n >= 0
+
+    if n <= 1:
+        return n
+
+    return fibonacci_number_naive(n - 1) + fibonacci_number_naive(n - 2)
+
+
+def fibonacci_number(n):
+    assert n >= 0
+    return n
+
+
+# TODO: when your solution is ready, uncomment the following three lines and submit to Coursera/edX
+# if __name__ == '__main__':
+#     n = int(input())
+#     print(fibonacci_number(n))
+
