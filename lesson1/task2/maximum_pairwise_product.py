d532e859c1c3a12b5a9d3faeaf49abc59de444b0
Alexander S. Kulikov <alexander.s.kulikov@gmail.com>
2018-04-23 16:14:08 +0300

    add new problem: maximum pairwise product

--- lesson1/task2/maximum_pairwise_product.py
+++ lesson1/task2/maximum_pairwise_product.py
@@ -1,1 +1,24 @@
-# TODO: type solution here
+#python3 (this comment is needed by the autograding system at Coursera and edX)
+
+def max_pairwise_product_naive(A):
+    assert len(A) >= 2
+
+    product = 0
+
+    for i in range(len(A)):
+        for j in range(i + 1, len(A)):
+            product = max(product, A[i] * A[j])
+
+    return product
+
+def max_pairwise_product(A):
+    type here
+
+# TODO: when your solution is ready, uncomment the following three lines and submit to Coursera/edX
+# if __name__ == '__main__':
+#     n = int(input())
+#     A = [int(x) for x in input().split()]
+#     assert len(A) == n
+#     print(max_pairwise_product(A))
+
+
