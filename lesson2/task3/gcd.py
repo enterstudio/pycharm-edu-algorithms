6817fac4c51a1d9d0a3241c5e336842cda0ee746
Alexander S. Kulikov <alexander.s.kulikov@gmail.com>
2018-04-26 12:29:42 +0300

    add new problem: gcd

---
+++ lesson2/task3/gcd.py
@@ -0,0 +1,21 @@
+#python3 (this comment is needed by the autograding system at Coursera and edX)
+
+def gcd_naive(a, b):
+    assert 0 <= a and a <= 2 * 10**9 and 0 <= b and b <= 20 * 10**9
+
+    for divisor in range(min(a, b), 0, -1):
+        if a % divisor == 0 and b % divisor == 0:
+            return divisor
+
+    assert False
+
+
+def gcd(a, b):
+    assert 0 <= a and a <= 2 * 10**9 and 0 <= b and b <= 20 * 10**9
+    type here
+
+
+# TODO: when your solution is ready, uncomment the following three lines and submit to Coursera/edX
+# if __name__ == '__main__':
+#     a, b = map(int, input().split())
+#     print(gcd(a, b))
